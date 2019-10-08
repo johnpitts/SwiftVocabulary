53b594cc2fe69ca98f17f9b37cd336ac0ec4f5ae
John Pitts <jgpitts@gmail.com>
2019-10-08 11:52:25 -0400

    added view controllers, created model for a word, added labels to definition viewController, hooked files up to storyboard

---
+++ SwiftVocabulary/SwiftVocabulary/VocabularyTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  VocabularyTableViewController.swift
+//  SwiftVocabulary
+//
+//  Created by John Pitts on 10/7/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+import UIKit
+
+class VocabularyTableViewController: UITableViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
---
+++ SwiftVocabulary/SwiftVocabulary/Word.swift
@@ -0,0 +1,15 @@
+//
+//  Word.swift
+//  SwiftVocabulary
+//
+//  Created by John Pitts on 10/7/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+import Foundation
+
+
+struct Word {
+    let name: String
+    let definition: String
+}
