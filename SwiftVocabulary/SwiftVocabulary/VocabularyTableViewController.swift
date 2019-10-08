699940c74ffbf157a870096eb85d5041dd2a8add
John Pitts <jgpitts@gmail.com>
2019-10-08 12:52:38 -0400

    added tableView datasource to achieve tableview cells displaying words. tests SAT.  fixed bug:  forgot to connect tableView file to storyboard tableView

--- SwiftVocabulary/SwiftVocabulary/VocabularyTableViewController.swift
+++ SwiftVocabulary/SwiftVocabulary/VocabularyTableViewController.swift
@@ -11,32 +11,30 @@ import UIKit
 class VocabularyTableViewController: UITableViewController {
     
     
-    
+    var words: [Word] = [Word(name: "Delegate", definition: "An object, such as a viewController class, who acts as a supplier of information to a delegator object"), Word(name: "Protocol", definition: "Set of rules which governs how an object performs at its basest level"), Word(name: "Object", definition: "A property or collection of properties which make up an overall bigger entity, which we call an object.  Objects can be classes, structs, or properties (variables, constants)")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
     
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        print("number of cells: \(words.count)")
+        return words.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+        print("cellForRowat")
+        cell.textLabel?.text = words[indexPath.row].name
 
         return cell
     }
@@ -46,7 +44,10 @@ class VocabularyTableViewController: UITableViewController {
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
+        if segue.identifier == "ShowDefinitions" {
+            
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        }
         // Pass the selected object to the new view controller.
     }
     
